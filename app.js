d51363eeb480ee788a86c9203fcad5db755b48d8
Dienul <haqdienul@gmail.com>
2019-10-03 16:57:31 +0700

    update
    morgan

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //require the express module
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
+const chalk = require('chalk')
 const dateTime = require("simple-datetime-formater");
 const bodyParser = require("body-parser");
 const chatRouter = require("./route/chatroute");
@@ -14,6 +16,24 @@ const io = require("socket.io");
 
 const port = 5000;
 
+// app.use(morgan('tiny'))
+const morganMiddleware = morgan(function (tokens, req, res) {
+  return [
+      // '\n\n\n',
+      // chalk.hex('#ff4757').bold('🍄  Morgan --> '),
+      chalk.hex('#34ace0').bold(tokens.method(req, res)),
+      chalk.hex('#ffb142').bold(tokens.status(req, res)),
+      chalk.hex('#ff5252').bold(tokens.url(req, res)),
+      // chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + ' ms'),
+      // chalk.hex('#f78fb3').bold('@ ' + tokens.date(req, res)),
+      chalk.yellow(tokens['remote-addr'](req, res)),
+      chalk.hex('#fffa65').bold('from ' + tokens.referrer(req, res)),
+      // chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
+      // '\n\n\n',
+  ].join(' ');
+});
+
+app.use(morganMiddleware)
 //bodyparser middleware
 app.use(bodyParser.json());
 
